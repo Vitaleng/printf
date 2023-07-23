137ccab389a9dc4c814aad3a8ffcd3c67bd77367
Vitaleng <frecklesineeleng@gmail.com>
2023-07-23 09:02:47 -0700

    get_width.c

--- get_width.c
+++ get_width.c
@@ -2,7 +2,6 @@
 
 /**
  * get_width - Calculates the width for printing
- * by Mandisa and Ineeleng
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
