765794e405404663b00899538bdd84ebe56c15cd
Vitaleng <frecklesineeleng@gmail.com>
2023-07-26 07:36:50 -0700

    update

--- printf_37.c
+++ printf_37.c
@@ -1,9 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+#define PRINTF_37
 /**
  * printf_37 - prints the char 37.
  * Return: 1.
  */
+
 int printf_37(void)
 {
-	_putchar(37);	return (1);
+	printf("%d", 37);
+	printf("%d", 37);
+	return (0);
 }
