b8dc59003c1d9911d96fd8023d4cd375dc6d1d96
Vitaleng <frecklesineeleng@gmail.com>
2023-07-26 08:05:39 -0700

    update

--- _printf.c
+++ _printf.c
@@ -6,14 +6,6 @@
  */
 int _printf(const char * const format, ...)
 {
-	#include "main.h"
-/**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
- */
-int _printf(const char * const format, ...)
-{
 	convert_match m[] = {
 		{"%s", printf_string}, {"%c", printf_char},
 		{"%%", printf_37},
